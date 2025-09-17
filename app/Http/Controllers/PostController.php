7ed422cd62781e7e382ddf621696045e97eb9a05
Muhammad Saiful Islam <islamsaiful154367@gmail.com>
2025-09-17 18:57:12 +0600

    Initial commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function create()
+    {
+        return view('create');
+    }
+    public function ourfilestore(Request $request)
+    {
+        $post = new Post;
+        $post->name = $request->name;
+        $post->description = $request->description;
+        $post->image = $request->image;
+        $post->save();
+        return redirect()->route('home')->with('success', 'Post created successfully.');
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp,container-queries"></script>
+
+    <style type="text/tailwindcss">
+        @layer utilities {
+      .container {
+            @apply px-10 mx-auto;
+        }
+    }
+  </style>
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="flex justify-between my-5">
+            <h2 class="text-red-500 text-xl">Home</h2>
+            <a href="/create" class="bg-green-600 text-white rounded py-2 px-4">Add New Post</a>
+        </div>
+        @if (session('success'))
+        <h2 class="text-green-600">{{session('success')}}</h2>
+        @endif
+    </div>
+
+</body>
+
+</html>
