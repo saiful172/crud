1598c67fe1dc060152959808dd9dcfaf744e0dbc
Muhammad Saiful Islam <islamsaiful154367@gmail.com>
2025-09-20 18:52:35 +0600

    crud-insert,update,delete operation done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,9 @@
       .container {
             @apply px-10 mx-auto;
         }
+        .btn{
+            @apply bg-green-600 text-white rounded py-2 px-4
+        }
     }
   </style>
     <title>Document</title>
@@ -20,11 +23,58 @@
     <div class="container">
         <div class="flex justify-between my-5">
             <h2 class="text-red-500 text-xl">Home</h2>
-            <a href="/create" class="bg-green-600 text-white rounded py-2 px-4">Add New Post</a>
+            <a href="/create" class="btn">Add New Post</a>
         </div>
         @if (session('success'))
         <h2 class="text-green-600">{{session('success')}}</h2>
         @endif
+        <div class="">
+            <div class="flex flex-col">
+                <div class="-m-1.5 overflow-x-auto">
+                    <div class="p-1.5 min-w-full inline-block align-middle">
+                        <div class="overflow-hidden">
+                            <table class="min-w-full divide-y divide-gray-200">
+                                <thead>
+                                    <tr>
+                                        <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Id</th>
+                                        <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Name</th>
+                                        <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Description</th>
+                                        <th scope="col" class="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase">Image</th>
+                                        <th scope="col" class="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($posts as $post )
+                                    <tr class="odd:bg-white even:bg-gray-100">
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">{{$post->id}}</td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">{{$post->name}}</td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">{{$post->description}}</td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
+                                            <img src="images/{{$post->image}}"
+                                                class="w-12 h-12 rounded-full object-cover"
+                                                alt="">
+                                        </td>
+
+                                        <td class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+                                            <a href="{{route('edit',$post->id)}}" class="btn">Edit</a>
+                                            <a href="{{route('delete',$post->id)}}" class="btn">Delete</a>
+
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                    
+
+
+
+
+                                </tbody>
+                            </table>
+                            {{$posts->links()}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
 </body>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
@@ -15,8 +16,11 @@ use App\Http\Controllers\PostController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', ['posts' => Post::paginate(5)]);
 })->name('home');
 
 Route::get('/create', [PostController::class, 'create']);
 Route::post('/store', [PostController::class, 'ourfilestore'])->name('store');
+Route::get('/edit/{id}', [PostController::class, 'editData'])->name('edit');
+Route::post('/update/{id}', [PostController::class, 'updateData'])->name('update');
+Route::get('/delete/{id}', [PostController::class, 'deleteData'])->name('delete');
