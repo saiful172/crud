fd775fe9638c930da3489215267e1e2e8e49ebb5
Muhammad Saiful Islam <islamsaiful154367@gmail.com>
2025-09-29 18:57:09 +0600

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,14 +33,14 @@
                 <div class="-m-1.5 overflow-x-auto">
                     <div class="p-1.5 min-w-full inline-block align-middle">
                         <div class="overflow-hidden">
-                            <table class="min-w-full divide-y divide-gray-200">
-                                <thead>
+                            <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700 border border-green-700 my-5">
+                                <thead class="bg-green-600">
                                     <tr>
-                                        <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Id</th>
-                                        <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Name</th>
-                                        <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Description</th>
-                                        <th scope="col" class="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase">Image</th>
-                                        <th scope="col" class="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase">Action</th>
+                                        <th scope="col" class="px-6 py-3 text-start text-xs font-medium uppercase">Id</th>
+                                        <th scope="col" class="px-6 py-3 text-start text-xs font-medium uppercase">Name</th>
+                                        <th scope="col" class="px-6 py-3 text-start text-xs font-medium uppercase">Description</th>
+                                        <th scope="col" class="px-6 py-3 text-end text-xs font-medium uppercase">Image</th>
+                                        <th scope="col" class="px-6 py-3 text-end text-xs font-medium uppercase">Action</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -57,12 +57,16 @@
 
                                         <td class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
                                             <a href="{{route('edit',$post->id)}}" class="btn">Edit</a>
-                                            <a href="{{route('delete',$post->id)}}" class="btn">Delete</a>
 
+                                            <form method="POST" action="{{route('delete',$post->id)}}" class="inline">
+                                                @csrf
+                                                @method('delete')
+                                                <button type="submit" class="bg-red-600 text-white rounded py-2 px-4 ">Delete</button>
+                                            </form>
                                         </td>
                                     </tr>
                                     @endforeach
-                                    
+
 
 
 
--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,4 @@ Route::get('/create', [PostController::class, 'create']);
 Route::post('/store', [PostController::class, 'ourfilestore'])->name('store');
 Route::get('/edit/{id}', [PostController::class, 'editData'])->name('edit');
 Route::post('/update/{id}', [PostController::class, 'updateData'])->name('update');
-Route::get('/delete/{id}', [PostController::class, 'deleteData'])->name('delete');
+Route::delete('/delete/{id}', [PostController::class, 'deleteData'])->name('delete');
